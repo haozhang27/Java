e23cb92b98d70a1561ba335afd7688a43749802b
haozhang27 <15592536102@163.com>
2019-08-29 12:06:02 +0800

    Java对类的学习和使用

---
+++ Learn0829.java
@@ -0,0 +1,178 @@
+﻿/*  在类中不构造方法，main方法引用类的操作
+//如何定义一个类
+class Person {
+	//定义属性
+	String name;
+	int age;
+	
+	//普通方法
+	void print() {
+		System.out.printf("name = %s, age = %d%n", name, age);
+	}
+}
+
+public class Learn0829 {
+	public static void main(String[] args) {
+		//只定义了一个引用变量，没有创建真正的对象
+		Person personNull = null;
+	
+		//如何根据类创建对象
+		Person person = new Person();
+		//使用对象
+		//对象.属性
+		//对象.方法
+		person.name = "中国人";   //引用类属性
+		person.age = 5000;		  //引用类属性
+		person.print();          //引用类方法
+		
+		Person person1 = new Person();
+		person1.name = "比特人";
+		person1.age = 4;
+		person1.print();
+		
+	}
+}*/
+/**
+ * java 类 Person
+ *
+ * @author haozhang
+ * @date 2019/08/29
+ */
+class Person {
+	//定义属性
+	String name;
+	int age;
+	
+	//构造方法
+	//1.在定义类的时候，没有明确的给出构造方法，Java编译器会
+	//补一个默认的构造方法出来 Person() {} 参数是空的，方法体是空的
+	//2.一旦明确定义过构造方法，不再有默认构造方法
+	Person(String n, int a) {
+		name = n;
+		age = a;
+	}
+	
+	//普通方法
+	void print() {
+		System.out.printf("name = %s, age = %d%n", name, age);
+	}
+}
+
+/**
+ * java 的类 Group
+ *
+ * @author haozhang
+ * @date 2019/08/29
+ */
+class Group {
+	//定义属性
+	String name;
+	int num;
+	
+	//构造方法：
+	//1.没有返回值类型
+	//2.方法名只能和类名相同
+	Group (String n, int m) {
+		name = n;
+		num = m;
+	}
+	
+	//普通方法：
+	void print (String welcome) {
+		System.out.printf("%s %s 的 %d 位学生%n", welcome, name, num);
+	}
+}
+
+/**
+ * java 的类 Number
+ *
+ * @author haozhang
+ * @date 2019/08/29
+ */
+class Number {
+	int sum;
+	Number (int a, int b, int c) {
+		System.out.println(a + b + c);
+		sum = a + b + c;
+	}
+	
+	void print() {
+		System.out.println(sum);
+	}
+}
+
+
+/**
+ * java 的类和对象
+ *
+ * @author haozhang
+ * @date 2019/08/29
+ */
+public class Learn0829 {
+	public static void main(String[] args) {
+		Person person = new Person("中国人", 5000);
+		person.print();
+		
+		Group group = new Group("人工智能组的", 50);
+		group.print("欢迎");
+		
+		Number number = new Number(1, 2, 3);
+		number.print();
+		
+		/*
+		//空引用 
+		Number number2 = null;
+		number2.print();    //NullPointerException  简称为NPE
+		*/
+	}
+}
+
+
+
+
+/*
+Java的类和对象
+
+1 面向对象的三大特性
+    1.封装 2.继承 3.多态
+
+2 如何定义类
+[限定符] class 类名称        [继承定义]             [接口实现]                   {类体 class body}   
+ public  class ClassName extends BaseClass implements Interface {
+	属性
+	方法
+	内部类
+} 
+
+现阶段在定义类时只要限定符和类名称
+public class ClassName {
+	fileds;       属性
+	methods;  方法
+}
+
+3 如何根据类创建对象
+类名称 变量名；
+变量名 = new 构造方法();
+
+4 限定符
+    public/private/什么都没有/protected
+
+    [限定符] class ClassName {}
+    public/什么都没有
+
+    public class Learn0708 {}	 公开
+    class Group {}                 	 包(package)内部
+
+    class ClassName {
+	[限定符]属性；	public/private/什么都没有
+	[限定符]方法；
+     }
+
+     访问限定符	类内部	包内部	不是同一个包的其他类
+     public		可访问	可访问	可访问
+     空白		可访问	可访问	不可访问
+     private		可访问	不可访问	不可访问
+一般情况下，设置成 private 会更好一点
+
+一个 Java 文件只能有一个 public 的类，而这个类的名称要和文件名一样，其他的类无所谓
+*/
---
+++ Practice0829.java
@@ -0,0 +1,183 @@
+﻿import java.util.Arrays;
+/**
+ * 对数组的练习
+ *
+ * @author haozhang
+ * @date 2019/08/29
+ */
+public class Practice0829 {
+
+	/**
+	 * 二分查找,查找前提是有序数组
+	 * @param array 要查找数组
+	 * @param key 要查找的元素
+	 * @return -1 表示没找到， 其他表示 key 在数组中的位置
+	 */
+	public static int binarySearch(int[] array, int key) {
+		int front = 0;
+		int last = array.length;
+
+		//在区间 [front, last) 上查找
+		while (front < last) {
+			int mid = front + (last - front) / 2;
+				if (key < array[mid]) {
+					//因为区间是左闭右开型的，所以要 比较 array[mid - 1] 右区间就要是 mid
+					last = mid;
+				}
+				else if (key > array[mid]) {
+					//左区间是 闭 的，所以就不需要再比较 array[mid] 直接比较 array[mid + 1]
+					front = mid + 1;
+				}
+				else {
+					return mid;
+				}
+		}
+		return -1;
+	}
+
+	/**
+	 * 交换 array[i] 和 array[j] 的位置，
+	 * 因为修改 array[i] 对应的修改就是引用共同指向的数据，所以修改可以生效
+	 * @param array 原数组
+	 * @param i 要交换的下标
+	 * @param j 要交换的下标
+	 */
+	public static void swap(int array[],  int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+
+	/**
+	 * 冒泡排序，减治算法
+	 * @param array
+	 */
+	public static void bubbleSort(int[] array) {
+		for (int i = 0; i < array.length; ++i) {
+			//需要经过 array.length 次冒泡数组就有序了
+			//冒泡过程就是在无序区间内，经过两两比较，把最大的数冒泡至最后
+			
+			boolean isSwapped = false;
+
+			/**
+			 * 无序部分：[0，array1.length - i)
+			 * 有序部分：[array1.length - i，array1.length)
+			 * 进行冒泡过程遍历整个无序区间
+			 * 因为 array[j] 和 array[j+1] 比较，所以 j这里需要减 1
+			 */
+			for (int j = 0; j < array.length - i - 1; j++) {
+				//冒泡过程中满足最大的数在最后面
+				if (array[j] > array[j + 1]) {
+					swap (array, j, j + 1);
+					isSwapped = true;
+				}
+			}
+			
+			//如果一次完整的冒泡排序过程中，一次交换都没发生，表明数组有序，排序完成
+			if (!isSwapped) {
+				return;
+			}
+		}
+	}
+	
+	
+	
+	public static void testPrint() {
+		//println  只输出一个参数  并且换行
+		System.out.println("中国人");
+		System.out.println(1);
+		System.out.println("中国人" + 1);
+		System.out.println();
+		
+		//print 只输出一个参数 但是不会换行
+		System.out.print("中国人");
+		System.out.print(1);
+		System.out.print("中国人" + 1);
+
+		//Java中的换行 是 %n
+		System.out.printf("我是中国人 %d %d%n", 1, 2);
+		System.out.format("我是中国人 %d %d%n", 1, 2);
+		
+	}
+
+	/**
+	 * 逆置数组
+	 * @param array 原数组
+	 * @return 逆置之后的数组
+	 */
+	public static int[] changeArray(int[] array) {
+		//数组的首元素地址
+		int front = 0;
+
+		//数组的尾元素地址
+		int last = array.length - 1;
+
+		//临时变量  用来存储 交换的值
+		int temp = 0;
+		while(front <= last) {
+			temp = array[front];
+			array[front] = array[last];
+			array[last] = temp;
+
+			//每逆置一次 首元素变为 (首元素 + 1) 的元素
+			front++;
+
+			//每逆置一次 尾元素变为 (尾元素 - 1) 的元素
+			last--;
+		}
+		return array;
+	}
+	
+	
+	public static void main(String[] args) {
+		//二分查找
+		int[] array = new int[] {1, 2, 3, 4, 5};
+		int index = binarySearch(array, 3);
+		System.out.println("3 所在的下标是 " + index);
+
+		//冒泡排序
+		int[] bubbleArray = new int[] {3, 2, 9, 7, 4};
+		bubbleSort(bubbleArray);
+		System.out.println(Arrays.toString(bubbleArray));
+
+		//数组逆置
+		int[] changeArray = changeArray(array);
+		System.out.println(Arrays.toString(changeArray));
+
+		//Java的几种输出命令
+		testPrint();
+	}
+}
+
+
+
+
+/*
+Java习题思路
+
+1 二分查找的思路
+    1.可以循环的条件是，front 和 last 确定的区间还有数，front < last
+    2.根据 front 和 last 计算中间下标的位置
+	int mid = (front + last) / 2；
+	int mid = front + (front - left) / 2；
+    3.比较 array[mid] 和 key (front,last)
+	1.array[mid] == key   return mid;
+	2.key < array[mid]      last = mid;
+	3.key > array[mid]      left = mid + 1;
+
+2 冒泡排序思路：
+     1.对一个数组 第一个 和 第二个比较 大的往后排
+     冒泡一次，可以让一个最大的数放到最后，一共经过 n 次 (n-1) 次
+
+3 减治算法
+    一次处理一个数，剩下的 n - 1 个数按相同方法处理
+
+4 关于Java的输出命令
+    System.out.println(String message);   //这个输出 message 并且自带输出换行
+    System.out.print(String message);     //这个只输出 message
+    无论 message 是什么类型，Java 都会将其转换为 String 类型
+
+    System.out.printf(String format, 变长的参数);
+    System.out.format(String format, 变长的参数);
+    前面指定格式，后面跟参数             %d %f %s
+*/
