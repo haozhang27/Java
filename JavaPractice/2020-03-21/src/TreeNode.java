a7c5303df06cd99f036c575237173423992de2a8
haozhang27 <15592536102@163.com>
2020-03-21 21:15:09 +0800

    Java练习

---
+++ JavaPractice/2020-03-21/src/TreeNode.java
@@ -0,0 +1,16 @@
+/**
+ * Demo class
+ *
+ * @author haozhang
+ * @date 2020/03/21
+ */
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+}
